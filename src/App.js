224099e3f2817790e9fb0dbf0dbe74936e8d58ba
luckybear0302 <davidhall0307@outlook.com>
2024-12-11 18:41:03 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+/* Pages */
+import RouterScrollTop from "./components/ScrollToTop/RouterScrollTop";
+
+function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
+  return (
+    <>
+      <RouterScrollTop />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import "./ScrolToTop.css";
+
+const ScrollToTop = () => {
+  const [backToTopButton, setBackTopButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setBackTopButton(true);
+      } else {
+        setBackTopButton(false);
+      }
+    });
+  }, []);
+
+  const ScrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className="App">
+      {backToTopButton && (
+        <button className="cm-up" onClick={ScrollUp}>
+          <span className="cm-icon">
+            <i className="fas fa-chevron-up"></i>
+          </span>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default React.memo(ScrollToTop);
